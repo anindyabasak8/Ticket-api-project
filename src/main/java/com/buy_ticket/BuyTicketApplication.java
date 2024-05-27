e177989c731a39b33937e50b47917e7ba3e06f6a
anindyabasak8 <anindyabasak8@gmail.com>
2024-05-27 18:56:45 +0530

    Second commit

--- src/main/java/com/buy_ticket/BuyTicketApplication.java
+++ src/main/java/com/buy_ticket/BuyTicketApplication.java
@@ -1,16 +1,24 @@
 package com.buy_ticket;
 
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
+
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
+import org.slf4j.*;
+
 @EnableTransactionManagement
 @SpringBootApplication
 public class BuyTicketApplication {
+	
+	public static Logger logger 
+    = LoggerFactory.getLogger(BuyTicketApplication.class); 
 
 	public static void main(String[] args) {
-		SpringApplication.run(BuyTicketApplication.class, args);
+		logger.info("Application started..");
+		SpringApplication.run(BuyTicketApplication.class, args);//hello
 	}
 
 }
